21d0c22bbe23c85427e4b5d9145e1b2e79c48993
eliseucostaalbano <56443711+eliseucostaalbano@users.noreply.github.com>
2024-06-26 14:11:47 -0300

    criando a peça de tetris

--- script.js
+++ script.js
@@ -7,7 +7,53 @@ let nextRandom = 0
 let timerId
 let score = 0
 
-// console.log(blocos)
+
 
 // as peças de tetris
 
+ const lTetromino = [
+   [1, width+1, width*2+1, 2],
+   [width, width+1, width+2, width*2+2],
+   [1, width+1, width*2+1, width*2],
+   [width, width+1, width+2, width*2+2]
+ ]
+   
+ const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
+const osTetrominos = [ lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+let posiçãoAtual = 4
+let atual = osTetrominos[0][0]
+
+// desenhando as peças
+
+
+
+
+// console.log(osTetrominos [0] [0])
